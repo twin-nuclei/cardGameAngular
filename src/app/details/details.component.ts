156dbebd15fa63828360c469f8e6d171729e697d
Kuebler, Philipp <philipp.kuebler@aoe.com>
2020-06-12 22:12:10 +0200

    made flow of player data work from parents (overview, details) to child (card)

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Player } from '../player';
+import {PLAYERS} from '../players';
 
 @Component({
   selector: 'app-details',
@@ -7,11 +8,7 @@ import { Player } from '../player';
   styleUrls: ['./details.component.sass']
 })
 export class DetailsComponent implements OnInit {
-  singleCard = {
-    realName: 'Pepa',
-    playerName: 'Wutz',
-    asset: 'Humor'
-  };
+  players = PLAYERS;
 
   constructor() { }
 
