c1433a6cd8186a4dc3bf32b761a251dd1775db90
Kuebler, Philipp <philipp.kuebler@aoe.com>
2020-06-12 11:49:41 +0200

    kickstarted app. added bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DetailsComponent } from './details/details.component';
+import {OverviewComponent} from './overview/overview.component';
+import { ControlsComponent } from './controls/controls.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OverviewComponent,
+    DetailsComponent,
+    ControlsComponent
   ],
   imports: [
     BrowserModule,
