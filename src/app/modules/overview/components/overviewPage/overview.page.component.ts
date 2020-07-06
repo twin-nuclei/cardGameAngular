01d985a4bb59082539490befade9174d798b7939
Kuebler, Philipp <philipp.kuebler@aoe.com>
2020-07-06 22:33:20 +0200

    overview now consumes the querry params set via controls to sort

--- src/app/modules/overview/components/overviewPage/overview.page.component.ts
+++ src/app/modules/overview/components/overviewPage/overview.page.component.ts
@@ -2,25 +2,30 @@ import {Component, OnInit} from '@angular/core';
 import {Player} from '../../../shared/interfaces/player';
 import {PlayersService} from '../../../shared/services/players.service';
 import {Observable} from 'rxjs';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-overview',
   templateUrl: './overview.page.component.html'
 })
 export class OverviewPageComponent implements OnInit {
-  constructor(private playerService: PlayersService) {}
+  constructor(private playerService: PlayersService,
+              private route: ActivatedRoute) {}
   players: Observable<Player[]> = this.playerService.getAllPlayers();
   sortOrder: string;
 
-  sortPlayers(ascending: boolean): void  {
-    if (ascending) {
-      this.sortOrder = 'realName';
-    } else {
-      this.sortOrder = '-realName';
-    }
-  }
-
-
   ngOnInit() {
+    this.route.queryParams
+      .subscribe(
+        (params) => {
+          if (params.sort === 'ascending') {
+            this.sortOrder = 'realName';
+          } else if (params.sort === 'ascending') {
+            this.sortOrder = '-realName';
+          } else {
+            this.sortOrder = null;
+          }
+        }
+    );
   }
 }
